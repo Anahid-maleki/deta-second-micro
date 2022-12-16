21cf192d94bafbec5487ff26a3a69fe52b1e2981
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-16 18:34:16 +0330

    second commit

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from flask import Flask
 app=Flask(__name__)
 @app.route('/<username>',methods=['GET'])
 def Hello_User(username):
-    return f"<html><body>Hello  {username}....</body></html>"
+    return f"<html><body>Hello and Welcome {username}....</body></html>"
     
