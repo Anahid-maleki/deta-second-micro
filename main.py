a52defcc6cff1585463867c10e83d8efa0a77ffe
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-16 18:54:33 +0330

    templates

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ app=Flask(__name__)
 @app.route('/<username>',methods=['GET'])
 def Hello_User(username):
     return f"<html><body>Hello and Welcome {username}....</body></html>"
+@app.route('/home',methodes=['GET'])
+def Home():
+    return render_template("text.html")    
     
