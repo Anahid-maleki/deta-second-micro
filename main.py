86f43828ce15eb1a0e063f0c7cba99345d4944b4
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-17 13:04:05 +0330

    add requests

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask,render_template
+import requests
+response=requests.get("https://api.github.com/User")
 app=Flask(__name__)
 @app.route('/<username>',methods=['GET'])
 def Hello_User(username):
     return f"<html><body>Hello and Welcome {username}....</body></html>"
 @app.route('/home',methods=['GET'])
 def Home():
-    return render_template("text.html")   
+    return render_template("text.html") 
+@app.route("/get user",methods=['GET'])
+def user_github():
+    return f"{response.json}"    
