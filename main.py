cbbae95124e44a65a77e29f6edc0f9889cf908e4
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-17 12:26:16 +0330

    trimming

--- main.py
+++ main.py
@@ -5,7 +5,4 @@ def Hello_User(username):
     return f"<html><body>Hello and Welcome {username}....</body></html>"
 @app.route('/home',methods=['GET'])
 def Home():
-    return render_template("text.html")
-@app.route('/pic',methods=['GET'])        
-def pic():
-    return f"<html><body><a<body></html>"    
+    return render_template("text.html")   
