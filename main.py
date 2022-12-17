c079298935a1450d470f44f5c0dfc185588bc02c
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-18 01:04:37 +0330

    added token

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
-from flask import Flask,render_template
-#import requests
+from flask import Flask,render_template,requests
+import urllib.request, json
+import os
 #response=requests.get("https://api.github.com/users")
 app=Flask(__name__)
 @app.route('/<username>',methods=['GET'])
 def Hello_User(username):
     return f"<html><body>Hello and Welcome {username}....</body></html>"
 @app.route('/home',methods=['GET'])
-def Home():
+def home():
     return render_template("text.html") 
 #@app.route("/info",methods=['GET'])
 #def user_github():
-#    return {response.json}    
+#    return {response.json}  
+@app.route("/info/<username>")
+def get_info(username):
+    url="https://api.github.com/users/<username>"
+    response=requests.get(url,headers={'athorization':"token{}".format(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)})
+    return f"{response.json}"
