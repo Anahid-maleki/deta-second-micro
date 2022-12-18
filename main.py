17c03b369039f2e96c97e12ef56a5cf84c051066
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-18 23:36:14 +0330

    changing Url

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def home():
 #    return {response.json}  
 @app.route("/info")
 def get_info():
-    url="https://api.github.com/users"
+    url="https://api.github.com/users/Anahid-maleki"
     response=requests.get(url,headers={'athorization':"token{}".format(my_key)})
     for x in response.json():
        return {x["name"]}
