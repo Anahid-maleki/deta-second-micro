b40087359abbb1d319b81d0443b959e0f2d63c23
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-19 21:53:22 +0330

    defult

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ import requests ,json
 #my_key=os.environ.get('API_key')
 #response=requests.get("https://api.github.com/users")
 app=Flask(__name__)
-#@app.route('/<username>',methods=['GET'])
-#def Hello_User(username):
-#    return f"<html><body>Hello and Welcome {username}....</body></html>"
-#@app.route('/home',methods=['GET'])
-#def home():
-#    return render_template("text.html") 
-#@app.route("/info",methods=['GET'])
-#def user_github():
-#    return {response.json}  
-@app.route("/",methods=["GET"])
-def get_info():
-    url="https://api.github.com/users/Anahid-maleki"
-    response=requests.get(url,headers={'my_token': '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'})
-    jsonData=response.json()
-    return jsonify(jsonData['id'])
+@app.route('/<username>',methods=['GET'])
+def Hello_User(username):
+    return f"<html><body>Hello and Welcome {username}....</body></html>"
+@app.route('/home',methods=['GET'])
+def home():
+    return render_template("text.html") 
+ 
+#@app.route("/",methods=["GET"])
+#def get_info():
+#    url="https://api.github.com/users/Anahid-maleki"
+    # response=requests.get(url,headers={'my_token': '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'})
+    # jsonData=response.json()
+    # return jsonify(jsonData['id'])
     
 
       
