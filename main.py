ac1f5cdfabb7dd5496e3191dbb497f3a19212f6a
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-19 21:41:10 +0330

    just one route

--- main.py
+++ main.py
@@ -4,16 +4,16 @@ import requests ,json
 #my_key=os.environ.get('API_key')
 #response=requests.get("https://api.github.com/users")
 app=Flask(__name__)
-@app.route('/<username>',methods=['GET'])
-def Hello_User(username):
-    return f"<html><body>Hello and Welcome {username}....</body></html>"
-@app.route('/home',methods=['GET'])
-def home():
-    return render_template("text.html") 
+#@app.route('/<username>',methods=['GET'])
+#def Hello_User(username):
+#    return f"<html><body>Hello and Welcome {username}....</body></html>"
+#@app.route('/home',methods=['GET'])
+#def home():
+#    return render_template("text.html") 
 #@app.route("/info",methods=['GET'])
 #def user_github():
 #    return {response.json}  
-@app.route("/info",methods=["GET"])
+@app.route("/",methods=["GET"])
 def get_info():
     url="https://api.github.com/users/Anahid-maleki"
     response=requests.get(url,headers={'my_token': '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'})
