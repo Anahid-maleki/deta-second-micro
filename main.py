d5b1cbc083ac5f4d5f44db6a3427e5c90cd607eb
Anahid-maleki <maleki.anahid@gmail.com>
2022-12-20 12:07:17 +0330

    making dynamic url

--- main.py
+++ main.py
@@ -8,17 +8,18 @@ password=os.getenv("my_token")
 #my_key=os.environ.get('API_key')
 #response=requests.get("https://api.github.com/users")
 app=Flask(__name__)
-@app.route('/<username>',methods=['GET'])
-def Hello_User(username):
-    return f"<html><body>Hello and Welcome my friend {username}....</body></html>"
+# @app.route('/<username>',methods=['GET'])
+# def Hello_User(username):
+#     return f"<html><body>Hello and Welcome my friend {username}....</body></html>"
 @app.route('/home',methods=['GET'])
 def home():
     return render_template("text.html") 
  
-@app.route("/",methods=["GET"])
-def get_info():
-   url="https://api.github.com/users/Anahid-maleki"
-   response=requests.get(url,headers={"my_token":"password"})
+@app.route("/<username>",methods=["GET"])
+def get_info(username):
+   url="https://api.github.com/users"
+   final_url= '/'.join([url, username])
+   response=requests.get(final_url,headers={"my_token":"password"})
    jsonData=response.json()
    return jsonify(jsonData)
     
